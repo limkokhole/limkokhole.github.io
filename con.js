0121a9c98cdbc6667148f05a10b43552e7e78db8
limkokhole <limkokhole@gmail.com>
2022-01-13 02:05:19 +0800

    anti img

--- con.js
+++ con.js
@@ -1,27 +1,20 @@
 var img = document.createElement("img");
-img.src = "con.jpg";
 let div = document.getElementById("x");
+img.src = "con.jpg";
 div.appendChild(img);
 img.src = "cOn.jpg";
-let div = document.getElementById("x");
 div.appendChild(img);
 img.src = "cON.jpg";
-let div = document.getElementById("x");
 div.appendChild(img);
 img.src = "coN.jpg";
-let div = document.getElementById("x");
 div.appendChild(img);
 img.src = "Con.jpg";
-let div = document.getElementById("x");
 div.appendChild(img);
 img.src = "COn.jpg";
-let div = document.getElementById("x");
 div.appendChild(img);
 img.src = "CnO.jpg";
-let div = document.getElementById("x");
 div.appendChild(img);
 img.src = "CNO.jpg";
-let div = document.getElementById("x");
 div.appendChild(img);
 
 
