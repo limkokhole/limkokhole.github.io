58499eb9a44e0fde755799065c03f60ddaccd996
limkokhole <limkokhole@gmail.com>
2022-01-13 01:57:46 +0800

    anti img

--- con.js
+++ con.js
@@ -1,5 +1,12 @@
 var img = document.createElement("img");
 img.src = "con.jpg";
+img.src = "cOn.jpg";
+img.src = "cON.jpg";
+img.src = "coN.jpg";
+img.src = "Con.jpg";
+img.src = "COn.jpg";
+img.src = "CnO.jpg";
+img.src = "CNO.jpg";
 
 var div = document.getElementById("x");
 div.appendChild(img);
