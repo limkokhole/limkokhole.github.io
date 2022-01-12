3ea815ce116588394449dbaf5a04731c8135aeb8
limkokhole <limkokhole@gmail.com>
2022-01-13 02:26:00 +0800

    more valid

--- con.js
+++ con.js
@@ -3,19 +3,25 @@ let div = document.getElementById("x");
 img.src = "con.jpg";
 div.appendChild(img);
 
-let img2 = document.createElement("img");
-img2.src = "cOn.jpg";
-div.appendChild(img2);
+img = document.createElement("img");
+img.src = "cOn.jpg";
+div.appendChild(img);
+img = document.createElement("img");
 img.src = "cON.jpg";
 div.appendChild(img);
+img = document.createElement("img");
 img.src = "coN.jpg";
 div.appendChild(img);
+img = document.createElement("img");
 img.src = "Con.jpg";
 div.appendChild(img);
+img = document.createElement("img");
 img.src = "COn.jpg";
 div.appendChild(img);
+img = document.createElement("img");
 img.src = "CoN.jpg";
 div.appendChild(img);
+img = document.createElement("img");
 img.src = "CON.jpg";
 div.appendChild(img);
 
